5cfe5e325a679bf007223bd9817ffac358621c33
shirayuki <shirayuki@46ki75.com>
2024-03-21 04:18:21 +0900

    refactor(code): remove unnecessary comments

--- src/app/api/blog/[slug]/info.json/saveImages.ts
+++ src/app/api/blog/[slug]/info.json/saveImages.ts
@@ -12,15 +12,12 @@ export async function saveImages(
       const arrayBuffer = await response.arrayBuffer()
       const buffer = Buffer.from(arrayBuffer)
 
-      // ディレクトリパスを構築（存在しない場合は作成）
       const dirPath = path.join(`./public/images/blog/${slug}`)
 
       fs.mkdirSync(dirPath, { recursive: true })
 
-      // 画像ファイルのパスを構築
       const filePath = path.join(dirPath, `i${url.index}.webp`)
 
-      // ダウンロードした画像をファイルとして保存
       fs.writeFileSync(filePath, buffer)
     } catch (error) {
       console.error(`Error downloading or saving image ${url.src}: `, error)
