8cdf007d3c9b88803c201949c37bbce5ee268468
shirayuki <shirayuki@46ki75.com>
2024-03-25 18:39:30 +0900

    feat(api): add test route to verify SSG functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module']
+  modules: ['@nuxtjs/eslint-module'],
+  routeRules: {
+    '/api/hello.json': { prerender: true }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>aaa</div>
+  <div>{{ data }}</div>
+</template>
+
+<script setup lang="ts">
+const data = ref({})
+onMounted(async () => {
+  const res = await fetch('/api/hello.json')
+  const body = await res.json()
+  data.value = body
+})
+</script>
+
+<style scoped></style>
