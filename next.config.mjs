8e5d1baaef5b08334df915cf8c0d6e6b6cc585ef
shirayuki <shirayuki@46ki75.com>
2024-03-25 17:35:16 +0900

    feat(migration): remove all files for migration process

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // FOR STATIC SITE GENERATION
-  output: 'export'
-}
-
-export default nextConfig
--- src/app/api/blog/[slug]/info.json/route.ts
+++
@@ -1,2 +0,0 @@
-export { GET } from './GET'
-export { generateStaticParams } from '../../generateStaticParams'
