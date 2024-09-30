57917b0040b85e563cf427d33834f7cedc021b3c
shirayuki <shirayuki@46ki75.com>
2024-09-30 23:50:24 +0900

    feat: Add header animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+'use client'
+
 import React from 'react'
 
 import styles from './Header.module.scss'
+import { useWindowScroll } from 'react-use'
 
 export const Header = () => {
-  return <header className={styles.header}>Header</header>
+  const { y } = useWindowScroll()
+
+  return (
+    <header
+      className={styles.header}
+      style={{
+        backgroundColor:
+          y > 50 ? 'rgba(255, 255, 255, 0.1)' : 'rgba(255, 255, 255, 0.8)'
+      }}
+    >
+      Header
+    </header>
+  )
 }
