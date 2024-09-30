f175349666545bb80f0efc9ccc07da063e3d09c8
shirayuki <shirayuki@46ki75.com>
2024-09-30 12:26:36 +0900

    feat: create Blog component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,8 +4,13 @@ import React from 'react'
 export default function Blog() {
   return (
     <>
-      <BlogMain createdAt={'2022-10-01'} updatedAt={'2024-9-30'}>
-        <>BLOG TOP PAGE CONTENTS (HARD CODING)</>
+      <BlogMain
+        title='BLOG TOP'
+        ogp='/static/blog/index-ogp.webp'
+        createdAt={'2022-10-01'}
+        updatedAt={'2024-9-30'}
+      >
+        <div>BLOG TOP PAGE CONTENTS (HARD CODING)</div>
       </BlogMain>
     </>
   )
