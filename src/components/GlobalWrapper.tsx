a4439a69b61562970bd18038aeca596ac831cbe9
shirayuki <shirayuki@46ki75.com>
2024-10-02 15:58:55 +0900

    feat: add react-query library

--- src/components/GlobalWrapper.tsx
+++ src/components/GlobalWrapper.tsx
@@ -18,20 +18,26 @@ const Pagetop = dynamic(() => import('relmethis').then((mod) => mod.Pagetop), {
   ssr: false
 })
 
+// react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const client = new QueryClient()
+
 export const GlobalWrapper = ({ children }: { children: ReactNode }) => {
   const isDark = useSelector((state: RootState) => state.theme.isDark)
 
   return (
     <NoSSR>
-      <div
-        className={clsx(styles.body, {
-          [styles['body--light']]: !isDark,
-          [styles['body--dark']]: isDark
-        })}
-      >
-        {children}
-      </div>
-      <Pagetop isDark={isDark} />
+      <QueryClientProvider client={client}>
+        <div
+          className={clsx(styles.body, {
+            [styles['body--light']]: !isDark,
+            [styles['body--dark']]: isDark
+          })}
+        >
+          {children}
+        </div>
+        <Pagetop isDark={isDark} />
+      </QueryClientProvider>
     </NoSSR>
   )
 }
