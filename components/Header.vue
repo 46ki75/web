1b869844d3f7b33ab4e7dcf6f5aa735a49222678
shirayuki <shirayuki@46ki75.com>
2024-11-09 03:04:25 +0900

    feat: update header component to include icons and improve layout with flexbox styling

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,14 @@
 <template>
-  <header class="header">HEADER</header>
+  <header class="header">
+    <div class="icon-container"></div>
+    <div class="icon-container"><ElmInlineText text="HEADER" /></div>
+    <div class="icon-container"><ElmToggleTheme /></div>
+  </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElmInlineText, ElmToggleTheme } from '@elmethis/core'
+</script>
 
 <style scoped lang="scss">
 .header {
@@ -12,5 +18,15 @@
   padding: 1rem;
   text-align: center;
   box-shadow: 0 0 0.125rem rgba(black, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+}
+
+.icon-container {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 }
 </style>
