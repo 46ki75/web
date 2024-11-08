5e225362d683213e828c14f7b9a7ec8e3c92ed58
shirayuki <shirayuki@46ki75.com>
2024-11-09 03:13:40 +0900

    feat: enhance Main component with breadcrumb and heading; import necessary Elm components

--- components/blog/Main.vue
+++ components/blog/Main.vue
@@ -1,7 +1,14 @@
 <template>
-  <main>MAIN</main>
+  <main>
+    <ElmBreadcrumb
+      :links="[{ text: 'Home' }, { text: 'Blog' }, { text: 'Article' }]"
+    />
+    <ElmHeading1 text="HEADING1 PLACEHOLDER" />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElmBreadcrumb, ElmHeading1 } from '@elmethis/core'
+</script>
 
 <style scoped lang="scss"></style>
