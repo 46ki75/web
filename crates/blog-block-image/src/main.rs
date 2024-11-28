0a684b97add14d89e2e701907cb00b873c1eed65
shirayuki <shirayuki@46ki75.com>
2024-11-29 05:54:05 +0900

    feat: implement fetch module with get_image_by_block_id and get_image_by_slug functions

--- crates/blog-block-image/src/main.rs
+++ crates/blog-block-image/src/main.rs
@@ -1,5 +1,7 @@
 use lambda_http::RequestExt;
 
+mod fetch;
+
 /// e.g.
 ///
 /// GET /?block_id={image_block_id}
@@ -33,7 +35,7 @@ async fn function_handler(
                 }
                 .to_string();
 
-                get_image_by_block_id(&block_id).await?
+                fetch::get_image_by_block_id(&block_id).await?
             }
             None => {
                 return Ok(lambda_http::Response::builder()
@@ -71,7 +73,7 @@ async fn function_handler(
             }
         };
 
-        get_image_by_slug(slug_number).await?
+        fetch::get_image_by_slug(slug_number).await?
     };
 
     let body = lambda_http::Body::Binary(bytes.to_vec());
@@ -85,70 +87,6 @@ async fn function_handler(
         .map_err(Box::new)?;
 
     Ok(response)
-}
-
-async fn get_image_by_block_id(
-    block_id: &str,
-) -> Result<bytes::Bytes, Box<dyn std::error::Error + Send + Sync>> {
-    dotenvy::dotenv().ok();
-
-    let notion_token = std::env::var("NOTION_API_KEY")?;
-    let client = notionrs::Client::new().secret(notion_token);
-
-    let request = client.get_block().block_id(block_id);
-    let response = request.send().await?;
-
-    let url = match response.block {
-        notionrs::block::Block::Image { image } => image.get_url(),
-        _ => return Err("image block not found".into()),
-    };
-
-    let client = reqwest::Client::new();
-    let response = client.get(url).send().await?;
-    let bytes = response.bytes().await?;
-
-    Ok(bytes)
-}
-
-async fn get_image_by_slug(
-    slug: u64,
-) -> Result<bytes::Bytes, Box<dyn std::error::Error + Send + Sync>> {
-    dotenvy::dotenv().ok();
-
-    let notion_token = std::env::var("NOTION_API_KEY")?;
-    let database_id = std::env::var("NOTION_BLOG_DATABASE_ID")?;
-
-    let client = notionrs::Client::new().secret(notion_token);
-
-    let filter = notionrs::filter::Filter::unique_id_equals("slug", slug);
-
-    let request = client
-        .query_database()
-        .database_id(database_id)
-        .filter(filter);
-    let response = request.send().await?;
-
-    let result = response.results.first().ok_or("image not found")?;
-
-    let images = result
-        .properties
-        .get("ogpImage")
-        .ok_or("ogpImage property not found")?;
-
-    let image = match images {
-        notionrs::page::PageProperty::Files(files) => {
-            files.files.first().ok_or("image not found")?
-        }
-        _ => return Err("image not found".into()),
-    };
-
-    let url = image.get_url();
-
-    let client = reqwest::Client::new();
-    let response = client.get(url).send().await?;
-    let bytes = response.bytes().await?;
-
-    Ok(bytes)
 }
 
 #[tokio::main]
