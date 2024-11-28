857298742223f0f2ad474f19d27d93ddab28c5e0
shirayuki <shirayuki@46ki75.com>
2024-11-29 07:07:04 +0900

    refactor: simplify Open Graph methods to use combinators for cleaner code

--- crates/graphql/src/resolvers/ogp.rs
+++ crates/graphql/src/resolvers/ogp.rs
@@ -29,57 +29,44 @@ impl Ogp {
     pub async fn title(&self) -> Option<String> {
         let document = scraper::Html::parse_document(&self.body);
         let selector = scraper::Selector::parse("title").unwrap();
-        let title_element = document.select(&selector).next();
-        let title = match title_element {
-            Some(element) => element.text().collect::<String>(),
-            None => return None,
-        };
-
-        Some(title)
+        document
+            .select(&selector)
+            .next()?
+            .text()
+            .next()
+            .map(|s| s.to_string())
     }
 
     pub async fn og_title(&self) -> Option<String> {
         let document = scraper::Html::parse_document(&self.body);
         let selector = scraper::Selector::parse("meta[property='og:title']").unwrap();
-        let og_title_element = document.select(&selector).next();
-        let og_title = match og_title_element {
-            Some(element) => {
-                let og_title = element.value().attr("content").unwrap_or_default();
-                Some(og_title.to_string())
-            }
-            None => None,
-        };
-
-        og_title
+        document
+            .select(&selector)
+            .next()?
+            .value()
+            .attr("content")
+            .map(|s| s.to_string())
     }
 
     pub async fn og_description(&self) -> Option<String> {
         let document = scraper::Html::parse_document(&self.body);
         let selector = scraper::Selector::parse("meta[property='og:description']").unwrap();
-        let og_description_element = document.select(&selector).next();
-        let og_description = match og_description_element {
-            Some(element) => {
-                let og_description = element.value().attr("content").unwrap_or_default();
-                Some(og_description.to_string())
-            }
-            None => None,
-        };
-
-        og_description
+        document
+            .select(&selector)
+            .next()?
+            .value()
+            .attr("content")
+            .map(|s| s.to_string())
     }
 
     pub async fn og_image(&self) -> Option<String> {
         let document = scraper::Html::parse_document(&self.body);
         let selector = scraper::Selector::parse("meta[property='og:image']").unwrap();
-        let og_image_element = document.select(&selector).next();
-        let og_image = match og_image_element {
-            Some(element) => {
-                let og_image = element.value().attr("content").unwrap_or_default();
-                Some(og_image.to_string())
-            }
-            None => None,
-        };
-
-        og_image
+        document
+            .select(&selector)
+            .next()?
+            .value()
+            .attr("content")
+            .map(|s| s.to_string())
     }
 }
