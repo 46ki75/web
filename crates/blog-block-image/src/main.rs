2701303e4ba20e2845aac61c27c6473b6acc5c35
shirayuki <shirayuki@46ki75.com>
2024-11-29 05:29:36 +0900

    feat: add 'bytes' dependency and enhance function handler to support slug query parameter

--- crates/blog-block-image/src/main.rs
+++ crates/blog-block-image/src/main.rs
@@ -9,7 +9,15 @@ async fn function_handler(
     dotenvy::dotenv().ok();
 
     let query_params = event.query_string_parameters();
+
     let block_id_query = query_params.all("block_id");
+    let slug_query = query_params.all("slug");
+
+    if block_id_query.is_none() && slug_query.is_none() {
+        return Ok(lambda_http::Response::builder()
+            .status(400)
+            .body("block_id or slug not found".into())?);
+    }
 
     let block_id = match block_id_query {
         Some(queries) => {
@@ -31,39 +39,11 @@ async fn function_handler(
         }
     };
 
-    let notion_token = std::env::var("NOTION_API_KEY")?;
-    let client = notionrs::Client::new().secret(notion_token);
-
-    let request = client.get_block().block_id(block_id);
-
-    let response = request.send().await?;
-
-    let url = match response.block {
-        notionrs::block::Block::Image { image } => image.get_url(),
-        _ => {
-            return Ok(lambda_http::Response::builder()
-                .status(400)
-                .body("image block not found".into())?)
-        }
-    };
-
-    let client = reqwest::Client::new();
-
-    let response = client.get(url).send().await?;
-
-    let headers = response.headers().clone();
-
-    let bytes = response.bytes().await?;
+    let bytes = get_image_by_block_id(&block_id).await?;
 
     let body = lambda_http::Body::Binary(bytes.to_vec());
 
-    let content_type = headers
-        .get("content-type")
-        .unwrap()
-        .to_str()
-        .unwrap_or_else(|_| {
-            infer::get(&bytes).map_or("application/octet-stream", |t| t.mime_type())
-        });
+    let content_type = infer::get(&bytes).map_or("application/octet-stream", |t| t.mime_type());
 
     let response = lambda_http::Response::builder()
         .status(200)
@@ -72,6 +52,29 @@ async fn function_handler(
         .map_err(Box::new)?;
 
     Ok(response)
+}
+
+async fn get_image_by_block_id(
+    block_id: &str,
+) -> Result<bytes::Bytes, Box<dyn std::error::Error + Send + Sync>> {
+    dotenvy::dotenv().ok();
+
+    let notion_token = std::env::var("NOTION_API_KEY")?;
+    let client = notionrs::Client::new().secret(notion_token);
+
+    let request = client.get_block().block_id(block_id);
+    let response = request.send().await?;
+
+    let url = match response.block {
+        notionrs::block::Block::Image { image } => image.get_url(),
+        _ => return Err("image block not found".into()),
+    };
+
+    let client = reqwest::Client::new();
+    let response = client.get(url).send().await?;
+    let bytes = response.bytes().await?;
+
+    Ok(bytes)
 }
 
 #[tokio::main]
