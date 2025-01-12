79daebb9314ef8d2f79d02b61ed205ca904b9c6c
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-12 19:24:59 +0900

    feat(api): add GraphQL support and define API endpoints

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,75 +1,101 @@
 use lambda_http::{http::Method, run, service_fn, tracing, Body, Error, Request, Response};
 use serde_json::json;
 
-mod query;
+mod graphql;
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     dotenvy::dotenv().ok();
 
     let schema = async_graphql::Schema::build(
-        query::QueryRoot,
+        graphql::query::QueryRoot,
         async_graphql::EmptyMutation,
         async_graphql::EmptySubscription,
     )
     .data(event.headers().clone())
     .finish();
 
-    if event.method() == Method::GET {
-        let playground_html = async_graphql::http::GraphiQLSource::build()
-            .endpoint("/lambda-url/api")
-            .finish();
-
-        let response = Response::builder()
-            .status(200)
-            .header("content-type", "text/html")
-            .body(playground_html.into())
-            .map_err(Box::new)?;
-        Ok(response)
-    } else if event.method() == Method::POST {
-        let request_body = event.body();
-
-        let gql_request = match serde_json::from_slice::<async_graphql::Request>(request_body) {
-            Ok(request) => request,
-            Err(err) => {
-                return Ok(Response::builder()
-                    .status(400)
-                    .header("content-type", "application/json")
-                    .body(
-                        json!({"error": format!("Invalid request body: {}", err)})
-                            .to_string()
-                            .into(),
-                    )
-                    .map_err(Box::new)?);
-            }
-        };
-
-        let gql_response = schema.execute(gql_request).await;
-
-        let response_body = match serde_json::to_string(&gql_response) {
-            Ok(body) => body,
-            Err(err) => {
-                return Ok(Response::builder()
-                    .status(500)
-                    .header("content-type", "application/json")
-                    .body(
-                        json!({"error": format!("Failed to serialize response: {}", err)})
-                            .to_string()
-                            .into(),
-                    )
-                    .map_err(Box::new)?);
-            }
-        };
-
+    if event.uri().path() == "/" {
         Ok(Response::builder()
             .status(200)
             .header("content-type", "application/json")
-            .body(response_body.into())
+            .body(include_str!("../endpoints.json").into())
             .map_err(Box::new)?)
+    } else if event.uri().path() == "/graphql" {
+        // # GraphQL ------------------------------
+
+        if event.method() == Method::GET {
+            let playground_html = async_graphql::http::GraphiQLSource::build()
+                .endpoint("/lambda-url/api/graphql")
+                .finish();
+
+            let response = Response::builder()
+                .status(200)
+                .header("content-type", "text/html")
+                .body(playground_html.into())
+                .map_err(Box::new)?;
+            Ok(response)
+        } else if event.method() == Method::POST {
+            let request_body = event.body();
+
+            let gql_request = match serde_json::from_slice::<async_graphql::Request>(request_body) {
+                Ok(request) => request,
+                Err(err) => {
+                    return Ok(Response::builder()
+                        .status(400)
+                        .header("content-type", "application/json")
+                        .body(
+                            json!({"error": format!("Invalid request body: {}", err)})
+                                .to_string()
+                                .into(),
+                        )
+                        .map_err(Box::new)?);
+                }
+            };
+
+            let gql_response = schema.execute(gql_request).await;
+
+            let response_body = match serde_json::to_string(&gql_response) {
+                Ok(body) => body,
+                Err(err) => {
+                    return Ok(Response::builder()
+                        .status(500)
+                        .header("content-type", "application/json")
+                        .body(
+                            json!({"error": format!("Failed to serialize response: {}", err)})
+                                .to_string()
+                                .into(),
+                        )
+                        .map_err(Box::new)?);
+                }
+            };
+
+            Ok(Response::builder()
+                .status(200)
+                .header("content-type", "application/json")
+                .body(response_body.into())
+                .map_err(Box::new)?)
+        } else {
+            let response = Response::builder()
+                .status(405)
+                .header("content-type", "application/json")
+                .body(json!({"error":"Method Not Allowed"}).to_string().into())
+                .map_err(Box::new)?;
+            Ok(response)
+        }
+    } else if event.uri().path() == "/api" {
+        // # REST ------------------------------
+
+        let response = Response::builder()
+            .status(200)
+            .header("content-type", "application/json")
+            .body(json!({"message":"Hello, world!"}).to_string().into())
+            .map_err(Box::new)?;
+        Ok(response)
     } else {
         let response = Response::builder()
-            .status(405)
+            .status(404)
             .header("content-type", "application/json")
-            .body(json!({"error":"Method Not Allowed"}).to_string().into())
+            .body(include_str!("../endpoints.json").into())
             .map_err(Box::new)?;
         Ok(response)
     }
