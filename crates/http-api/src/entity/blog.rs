66a6171ef7c3dc7fb83c46973ca3fd50759f893e
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-22 15:27:00 +0000

    docs: add doc comments to BlogEntity and related structs

--- crates/http-api/src/entity/blog.rs
+++ crates/http-api/src/entity/blog.rs
@@ -1,21 +1,50 @@
+#![deny(missing_docs)]
+//! Blog record returned by the BlogService.
+
+/// Blog record returned by the BlogService.
 pub struct BlogEntity {
+    /// Unique identifier of the blog.
     pub id: String,
+
+    /// Slug of the blog. (Currently unused)
     pub slug: String,
+
+    /// Title of the blog.
     pub title: String,
+
+    /// Description of the blog.
     pub description: String,
+
+    /// URL of the OGP image. The signed URL expires in 1 hour.
     pub ogp_image_s3_url: Option<String>,
+
+    /// Tags associated with the blog.
     pub tags: Vec<BlogTagEntity>,
+
+    /// Status of the blog. Only `Published` blogs are returned.
     pub status: BlogStatusEntity,
+
+    /// RFC 3339-formatted creation timestamp.
     pub created_at: String,
+
+    /// RFC 3339-formatted last update timestamp.
     pub updated_at: String,
 }
 
+/// Blog tag record returned by the BlogService.
 pub struct BlogTagEntity {
+    /// Unique identifier of the blog tag.
     pub id: String,
+
+    /// Name of the blog tag.
     pub name: String,
+
+    /// Color of the blog tag.
     pub color: BlogTagColorEntity,
 }
 
+/// Color opstions for a blog tag.
+#[allow(missing_docs)]
 #[derive(Debug, Default)]
 pub enum BlogTagColorEntity {
     #[default]
@@ -31,6 +60,8 @@ pub enum BlogTagColorEntity {
     Yellow,
 }
 
+/// Status options for a blog.
+#[allow(missing_docs)]
 #[derive(Debug, Default)]
 pub enum BlogStatusEntity {
     #[default]
