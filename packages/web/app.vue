f3b6667ade3984bd9594396475de63004b0ba0de
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-25 14:58:47 +0000

    feat: add initial layout and index page, update app structure, and include core dependencies

--- packages/web/app.vue
+++ packages/web/app.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+
+  transition: background-color 200ms;
+
+  background-color: #f2f2f2;
+
+  [data-theme="dark"] & {
+    background-color: #262626;
+  }
+}
+</style>
---
+++ packages/web/layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <div></div>
+    <NuxtLink to="/" class="to-home">
+      <ElmInlineText text="HOME" />
+    </NuxtLink>
+    <ElmToggleTheme />
+  </header>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ElmInlineText, ElmToggleTheme } from "@elmethis/core";
+</script>
+
+<style lang="scss" scoped>
+.header {
+  box-sizing: border-box;
+  padding: 0.5rem;
+  height: 3.5rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: rgba(grey, 0.1);
+}
+
+.to-home {
+  all: unset;
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  opacity: 1;
+  cursor: pointer;
+
+  transition: background-color 100ms, opacity 100ms;
+
+  &:hover {
+    background-color: rgba(#6987b8, 0.1);
+  }
+
+  &:active {
+    opacity: 0.5;
+  }
+}
+</style>
