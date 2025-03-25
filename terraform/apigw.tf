6e7ebc24cb67d9774d3c0ae8220a591f709ffe6a
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-25 13:05:36 +0000

    feat: update API Gateway configuration to use proxy integration and modify route settings

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -1,30 +1,14 @@
 resource "aws_apigatewayv2_api" "backend" {
   name          = "${terraform.workspace}-46ki75-web-apigwv2-http-backend"
   protocol_type = "HTTP"
-}
 
-resource "aws_apigatewayv2_integration" "backend" {
-  api_id           = aws_apigatewayv2_api.backend.id
-  integration_type = "AWS_PROXY"
-
-  connection_type = "INTERNET"
-  integration_uri = aws_lambda_alias.http_api.invoke_arn
-
-  timeout_milliseconds = 29 * 1000
-
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "backend" {
-  api_id             = aws_apigatewayv2_api.backend.id
-  route_key          = "ANY /"
-  target             = "integrations/${aws_apigatewayv2_integration.backend.id}"
-  authorization_type = "NONE"
+  route_key = "ANY /{proxy+}"
+  target    = aws_lambda_alias.http_api.invoke_arn
 }
 
 resource "aws_apigatewayv2_stage" "backend" {
   api_id      = aws_apigatewayv2_api.backend.id
-  name        = terraform.workspace
+  name        = "api"
   auto_deploy = true
 
   route_settings {
