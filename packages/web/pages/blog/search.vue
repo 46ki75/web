95ecd6acd6408e8d62dbda41df0320ef50b4497d
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-26 11:30:55 +0000

    feat: add search page with keyword input and routing functionality

---
+++ packages/web/pages/blog/search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <BlogMeta
+      title="Search"
+      created-at="2022-10-01"
+      updated-at="2025-03-26"
+      :links="[
+        { text: 'Home', href: '/' },
+        { text: 'Blog', href: '/blog' },
+        { text: 'Search', href: '/search' },
+      ]"
+    />
+
+    <ElmTextField v-model="keyword" label="検索キーワード" :icon="SearchIcon" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ElmTextField } from "@elmethis/core";
+import { Icon } from "@iconify/vue";
+
+const route = useRoute();
+const router = useRouter();
+
+const keyword = ref<string | undefined>();
+
+const SearchIcon = h(Icon, { icon: "material-symbols:search" });
+
+watch(keyword, (newKeyword) => {
+  router.replace({
+    query: {
+      keyword: newKeyword,
+    },
+  });
+});
+</script>
