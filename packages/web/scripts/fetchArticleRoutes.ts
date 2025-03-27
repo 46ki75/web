6b18a562348a3ac302d16681b16ddcf1147dc8a9
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-27 14:37:03 +0000

    refactor: remove directory management from fetchArticleRoutes

--- packages/web/scripts/fetchArticleRoutes.ts
+++ packages/web/scripts/fetchArticleRoutes.ts
@@ -1,15 +1,4 @@
-import { statSync, mkdirSync, rmSync } from "node:fs";
-
 export const fetchArticleRoutes = async (endpoint: string) => {
-  const stat = statSync("public/_notion");
-
-  if (stat.isDirectory()) {
-    rmSync("public/_notion", { recursive: true, force: true });
-  }
-
-  mkdirSync("public/_notion/blog/image/ogp", { recursive: true });
-  mkdirSync("public/_notion/blog/image/block", { recursive: true });
-
   const res = await fetch(`${endpoint}/api/graphql`, {
     method: "POST",
     body: JSON.stringify({
