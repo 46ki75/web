42d12c5d228bfbd3ae75e2df78b63f89ae721280
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-27 17:20:38 +0000

    feat(animation): enhance transition effects with transform for smoother entry and exit

--- packages/web/app.vue
+++ packages/web/app.vue
@@ -19,16 +19,18 @@ useSeoMeta({
 .v-enter-to,
 .v-leave-from {
   opacity: 1;
+  transform: translateY(0);
 }
 
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 150ms;
+  transition: opacity 150ms, transform 150ms;
 }
 
 .v-enter-from,
 .v-leave-to {
   opacity: 0;
+  transform: translateY(-16px);
 }
 </style>
 
--- packages/web/pages/blog.vue
+++ packages/web/pages/blog.vue
@@ -10,15 +10,17 @@
 .v-enter-to,
 .v-leave-from {
   opacity: 1;
+  transform: translateY(0);
 }
 
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 150ms;
+  transition: opacity 150ms, transform 150ms;
 }
 
 .v-enter-from,
 .v-leave-to {
   opacity: 0;
+  transform: translateY(-16px);
 }
 </style>
