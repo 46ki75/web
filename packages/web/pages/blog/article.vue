21877b26c06e92409f4565c17e0e376bb4d712c2
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-27 18:31:49 +0000

    feat(blog): add transition effects to NuxtPage for smoother article loading

--- packages/web/pages/blog/article.vue
+++ packages/web/pages/blog/article.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <NuxtPage />
+    <NuxtPage :transition="{ mode: 'out-in' }" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 150ms, transform 150ms;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-16px);
+}
+</style>
