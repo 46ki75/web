ab207bd0203ce19784e9d705131d22eaed2a722a
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-27 15:02:44 +0000

    feat: add deployment and cache invalidation scripts for S3

---
+++ packages/web/scripts/invalidate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+if [ -z "$STAGE_NAME" ]; then
+    echo "STAGE_NAME is not set"
+    exit 1
+fi
+
+if [ "$STAGE_NAME" = "prod" ]; then
+    DISTRIBUTION_ALIAS_DOMAIN="internal.46ki75.com"
+else
+    DISTRIBUTION_ALIAS_DOMAIN="${STAGE_NAME}-www.46ki75.com"
+fi
+
+echo "Invalidating cache for $DISTRIBUTION_ALIAS_DOMAIN"
+
+DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?Aliases.Items[?@ == '$DISTRIBUTION_ALIAS_DOMAIN']].Id" --output text)
+
+if [ -z "$DISTRIBUTION_ID" ]; then
+    echo "Distribution ID not found"
+    exit 1
+else
+    echo "Distribution ID: $DISTRIBUTION_ID"
+    INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/*" --query "Invalidation.Id" --output text)
+    echo "Invalidation ID: $INVALIDATION_ID"
+fi
