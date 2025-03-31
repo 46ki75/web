8892305650e0d7973f3ba0ce72dd2db1f388ebb5
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-31 03:54:41 +0000

    feat: add loading indicator with Elm components to enhance user experience

--- packages/web/pages/index.vue
+++ packages/web/pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { ElmButton } from "@elmethis/core";
+
+const { start, finish } = useLoadingIndicator();
+</script>
+
 <template>
   <div key="/">
     <div>
@@ -5,6 +11,9 @@
       <div>
         <NuxtLink to="/blog">Blog</NuxtLink>
       </div>
+
+      <ElmButton block @click="start">LOADING START</ElmButton>
+      <ElmButton block @click="finish">LOADING STOP</ElmButton>
     </div>
   </div>
 </template>
