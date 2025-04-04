6eef994a7ad8e433953e3fbaa5c2cfdb320b0020
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-04 15:19:42 +0000

    feat: add partitioning support to Glue catalog table for CloudFront data

--- terraform/aws/glue.tf
+++ terraform/aws/glue.tf
@@ -5,6 +5,24 @@ resource "aws_glue_catalog_table" "cloudfront" {
   name          = "${terraform.workspace}-46ki75-web-glue-table-cloudfront"
   database_name = aws_glue_catalog_database.default.name
 
+  partition_keys {
+    name = "partition_date"
+    type = "string"
+  }
+
+  // @see <https://docs.aws.amazon.com/ja_jp/athena/latest/ug/partition-projection-supported-types.html>
+  parameters = {
+    "projection.enabled" = "true",
+
+    "projection.partition_date.format"        = "yyyy/MM/dd/HH",
+    "projection.partition_date.interval"      = "1",
+    "projection.partition_date.interval.unit" = "HOURS",
+    "projection.partition_date.range"         = "2025/04/03/00,NOW",
+    "projection.partition_date.type"          = "date",
+
+    "storage.location.template" = "s3://${aws_s3_bucket.cloudfront.bucket}/$${partition_date}",
+  }
+
   storage_descriptor {
     location      = "s3://${aws_s3_bucket.cloudfront.bucket}/"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
