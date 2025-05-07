d0ffe42a3cee24d2bc450c6030aa9536c81bce0b
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-07 14:13:52 +0000

    fix: remove ElmParallax component from blog layout and add background styling to default layout

--- packages/web/layouts/default.vue
+++ packages/web/layouts/default.vue
@@ -1,9 +1,36 @@
 <template>
   <BaseHeader />
 
-  <div>
+  <div class="bg">
     <slot />
+    <ElmParallax
+      image-url1="/image/blog/bg1.webp"
+      image-url2="/image/blog/bg2.webp"
+    />
   </div>
 
   <BaseFooter />
 </template>
+
+<script setup lang="ts">
+import { ElmParallax } from "@elmethis/core";
+</script>
+
+<style scoped lang="scss">
+.bg {
+  background-image: radial-gradient(
+    circle,
+    rgba(#25282e, 0.05) 10%,
+    transparent 10%
+  );
+  background-size: 20px 20px;
+
+  [data-theme="dark"] & {
+    background-image: radial-gradient(
+      circle,
+      rgba(#bec2ca, 0.04) 10%,
+      transparent 10%
+    );
+  }
+}
+</style>
