aa18c9e5af155afb03cad4a03615696a0b5c4aff
Ikuma Yamashita <me@ikuma.cloud>
2025-06-02 16:06:53 +0000

    fix: handle missing pages in sitemap generation

--- packages/web/scripts/prepare.ts
+++ packages/web/scripts/prepare.ts
@@ -1,10 +1,17 @@
 import { fetchBlogList } from "./fetchBlogList";
+import { ENDPOINT } from "./fetchConfig";
 import { fetchImages } from "./fetchImages";
+import { fetchPrerenderRoutes } from "./fetchRoutes";
 import { generateRobots } from "./generateRobots";
 import { generateSitemap } from "./generateSitemap";
 
 export const BLOGS = await fetchBlogList();
+const routes = await fetchPrerenderRoutes(ENDPOINT);
 
-const promises = [fetchImages(BLOGS), generateRobots(), generateSitemap(BLOGS)];
+const promises = [
+  fetchImages(BLOGS),
+  generateRobots(),
+  generateSitemap(routes),
+];
 
 await Promise.all(promises);
