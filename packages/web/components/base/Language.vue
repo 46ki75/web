1323c97b82db1f3d7a1da7fbc537b04dee4543be
Ikuma Yamashita <me@ikuma.cloud>
2025-06-02 23:57:41 +0000

    feat: refactor Language component structure; add cover transition for locale toggle

--- packages/web/components/base/Language.vue
+++ packages/web/components/base/Language.vue
@@ -1,31 +1,50 @@
 <template>
-  <div :class="$style.language" @click="toggle">
+  <div class="language" @click="toggle">
     <ElmMdiIcon :d="mdiTranslate" />
-    <div :class="$style['language-name']">
+    <div class="language-name">
       <ElmInlineText
         v-if="localeProperties.name"
         :text="localeProperties.name"
       />
     </div>
   </div>
+
+  <Teleport to="body">
+    <Transition>
+      <div v-if="showCover" class="cover"></div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import { ElmInlineText, ElmMdiIcon } from "@elmethis/core";
 import { mdiTranslate } from "@mdi/js";
 
+const showCover = ref(false);
+
+const sleep = (duration: number) =>
+  new Promise((resolve) => window.setTimeout(resolve, duration));
+
 const { locale, localeProperties, setLocale } = useI18n();
 
 const toggle = async () => {
+  showCover.value = true;
+
+  await sleep(200);
+
   if (locale.value === "en") {
     setLocale("ja");
   } else {
     setLocale("en");
   }
+
+  await sleep(200);
+
+  showCover.value = false;
 };
 </script>
 
-<style module lang="scss">
+<style scoped lang="scss">
 .language {
   display: flex;
   flex-direction: row;
@@ -48,5 +67,34 @@ const toggle = async () => {
   justify-content: center;
   align-items: center;
   width: 3.5rem;
+}
+
+.cover {
+  position: fixed;
+  z-index: 10000;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100vh;
+  pointer-events: none;
+  background-color: #dee0e4;
+
+  [data-theme="dark"] & {
+    background-color: #3e434b;
+  }
+}
+
+.v-leave-from {
+  opacity: 1;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 200ms;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
