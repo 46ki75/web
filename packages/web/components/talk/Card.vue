fe6625b0ff3b7f482245097f9fcb3ea192aa243b
Ikuma Yamashita <me@ikuma.cloud>
2025-08-01 15:23:58 +0000

    feat: add Talks page and card component with localization support

---
+++ packages/web/components/talk/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nuxt-link :to="url">
+      <nuxt-img :src="ogp" />
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NuxtImg } from "#components";
+
+export interface TalkCardProps {
+  /**
+   * Link to the presentation slides.
+   */
+  url: string;
+
+  /**
+   * Date when the talk is presented.
+   */
+  date: string;
+
+  /**
+   * URL of the Open Graph image.
+   */
+  ogp: string;
+
+  /**
+   * Language in which the talk is presented.
+   */
+  language: "en" | "ja";
+}
+
+withDefaults(defineProps<TalkCardProps>(), {});
+</script>
+
+<style module lang="scss">
+/** PLACEHOLDER */
+</style>
---
+++ packages/web/pages/talks.vue
@@ -0,0 +1,43 @@
+<template>
+  <BaseContainer>
+    <ElmBreadcrumb
+      :links="[
+        {
+          text: 'Home',
+          onClick: () =>
+            $router.push(locale === defaultLocale ? `/` : `/${locale}`),
+        },
+        {
+          text: 'Talks',
+          onClick: () =>
+            $router.push(
+              locale === defaultLocale ? `/talks` : `/${locale}/talks`
+            ),
+        },
+      ]"
+    />
+
+    <BaseDate created-at="2025-08-01" updated-at="2025-08-01" />
+
+    <ElmHeading>
+      {{ t("talks.title") }}
+    </ElmHeading>
+
+    <ElmParagraph>
+      {{ t("talks.description") }}
+    </ElmParagraph>
+  </BaseContainer>
+</template>
+
+<script setup lang="ts">
+import { ElmHeading, ElmBreadcrumb, ElmParagraph } from "@elmethis/core";
+const { locale, defaultLocale, t } = useI18n();
+</script>
+
+<style module lang="scss">
+.talks-container {
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+}
+</style>
