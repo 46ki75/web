848018fc33a1421e7860f72f579391f2b100824a
Ikuma Yamashita <me@ikuma.cloud>
2025-08-29 17:07:26 +0000

    fix: add WASM Image Converter link to footer and update index page layout

--- packages/web/pages/index.vue
+++ packages/web/pages/index.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
-import { ElmButton, ElmKatex } from "@elmethis/core";
-
-const { start, finish } = useLoadingIndicator();
+import { ElmCubeIcon } from "@elmethis/core";
+const { locale } = useI18n();
+await navigateTo(locale.value === "en" ? "/blog" : `/${locale.value}/blog`);
 </script>
 
 <template>
   <div key="/">
-    <div>
-      <div>INDEX</div>
-      <ElmKatex expression="x = \frac{ -b \pm \sqrt{b^2 - 4ac} }{2a}" block />
-      <div>
-        <NuxtLinkLocale to="/blog">Blog</NuxtLinkLocale>
-      </div>
-
-      <div>
-        <NuxtLinkLocale to="/image-converter">Image Converter</NuxtLinkLocale>
-      </div>
-
-      <ElmButton block @click="start">LOADING START</ElmButton>
-      <ElmButton block @click="finish">LOADING STOP</ElmButton>
+    <div :class="$style.empty">
+      <ElmCubeIcon />
     </div>
   </div>
 </template>
+
+<style module lang="scss">
+.empty {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
