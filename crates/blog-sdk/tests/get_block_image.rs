00d608b262c99cb1d4bf7dc81d009e048b8951e3
Ikuma Yamashita <me@ikuma.cloud>
2025-10-11 13:29:58 +0000

    feat: add test for get_block_image function in blog-sdk

---
+++ crates/blog-sdk/tests/get_block_image.rs
@@ -0,0 +1,14 @@
+#[tokio::test]
+async fn get_block_image() {
+    dotenvy::dotenv().unwrap();
+
+    let notion_api_key = std::env::var("NOTION_API_KEY").unwrap();
+    let blog_image_block_id = std::env::var("BLOG_IMAGE_BLOCK_ID").unwrap();
+
+    let notionrs_client = notionrs::Client::new(notion_api_key);
+
+    let _bytes =
+        blog_sdk::command::get_block_image::get_block_image(notionrs_client, &blog_image_block_id)
+            .await
+            .unwrap();
+}
