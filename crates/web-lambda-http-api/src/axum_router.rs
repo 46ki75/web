a3e6537f7871745dbbc2c2170d6886a9fb530375
Ikuma Yamashita <me@ikuma.cloud>
2025-10-12 10:33:40 +0000

    refactor: move health check handler to a separate function for clarity

--- crates/web-lambda-http-api/src/axum_router.rs
+++ crates/web-lambda-http-api/src/axum_router.rs
@@ -7,23 +7,22 @@ pub async fn init_router() -> anyhow::Result<&'static axum::Router> {
     ROUTER
         .get_or_try_init(|| async {
             let app = axum::Router::new()
-                .route(
-                    "/api/health",
-                    axum::routing::get(|| async {
-                        #[derive(serde::Serialize)]
-                        struct Status {
-                            status: String,
-                        }
-
-                        axum::Json(Status {
-                            status: "ok".to_string(),
-                        })
-                    }),
-                )
+                .route("/api/health", axum::routing::get(handle_health_check))
                 .layer(tower_http::normalize_path::NormalizePathLayer::trim_trailing_slash())
                 .layer(tower_http::catch_panic::CatchPanicLayer::new());
 
             Ok(app)
         })
         .await
+}
+
+async fn handle_health_check() -> impl axum::response::IntoResponse {
+    #[derive(serde::Serialize)]
+    struct Status {
+        status: String,
+    }
+
+    axum::Json(Status {
+        status: "ok".to_string(),
+    })
 }
