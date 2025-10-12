c46bb1adc71839fc3d8b695533fb97da88d695c4
Ikuma Yamashita <me@ikuma.cloud>
2025-10-12 09:52:29 +0000

    chore: rename sub crate name to web-lambda-http-api

--- crates/web-lambda-http-api/src/main.rs
+++ crates/web-lambda-http-api/src/main.rs
@@ -1,11 +1,11 @@
-use http_api;
+use web_lambda_http_api;
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_http::Error> {
     lambda_http::tracing::init_default_subscriber();
 
     lambda_http::run(lambda_http::service_fn(
-        http_api::http_handler::function_handler,
+        web_lambda_http_api::http_handler::function_handler,
     ))
     .await
 }
