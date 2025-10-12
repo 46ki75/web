4867e7117f79d8ac9d89e95d37522b30318a892f
Ikuma Yamashita <me@ikuma.cloud>
2025-10-12 14:00:26 +0000

    fix: improve error messages for environment variable and SSM parameter errors

--- crates/web-lambda-http-api/src/error.rs
+++ crates/web-lambda-http-api/src/error.rs
@@ -1,14 +1,16 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("environment variable not found: `{variable_name}`")]
+    #[error("EnvironmentVariableNotFound: environment variable not found: `{variable_name}`")]
     EnvironmentVariableNotFound { variable_name: String },
 
-    #[error("failed to fetch parameter: `{parameter_name}`, trace: {trace}")]
+    #[error(
+        "SsmParameterApiFailed: failed to fetch parameter: `{parameter_name}`, trace: {trace}"
+    )]
     SsmParameterApiFailed {
         parameter_name: String,
         trace: String,
     },
 
-    #[error("farameter not found: `{parameter_name}`")]
+    #[error("SsmParameterNotFound: farameter not found: `{parameter_name}`")]
     SsmParameterNotFound { parameter_name: String },
 }
