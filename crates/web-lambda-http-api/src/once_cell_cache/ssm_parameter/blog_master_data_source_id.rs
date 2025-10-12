549356be8cde73c2644264277267fe6ff3470ad8
Ikuma Yamashita <me@ikuma.cloud>
2025-10-12 13:49:36 +0000

    fix: update return type of init_blog_master_data_source_id to use crate error type

--- crates/web-lambda-http-api/src/once_cell_cache/ssm_parameter/blog_master_data_source_id.rs
+++ crates/web-lambda-http-api/src/once_cell_cache/ssm_parameter/blog_master_data_source_id.rs
@@ -2,7 +2,7 @@ use tokio::sync::OnceCell;
 
 static BLOG_MASTER_DATA_SOURCE_ID: OnceCell<String> = OnceCell::const_new();
 
-pub async fn init_blog_master_data_source_id() -> anyhow::Result<&'static String> {
+pub async fn init_blog_master_data_source_id() -> Result<&'static String, crate::error::Error> {
     BLOG_MASTER_DATA_SOURCE_ID
         .get_or_try_init(|| async {
             let parameter = super::try_get_ssm_parameter_async("parameter_name").await?;
