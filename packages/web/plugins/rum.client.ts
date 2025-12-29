a56215d2b633d24fd314bad61d8068b3552031fe
Ikuma Yamashita <me@ikuma.cloud>
2025-12-29 15:02:43 +0000

    fix: move configuration initialization inside hostname check for production environment

--- packages/web/plugins/rum.client.ts
+++ packages/web/plugins/rum.client.ts
@@ -1,19 +1,21 @@
 import { AwsRum } from "aws-rum-web";
 
 export default defineNuxtPlugin(async () => {
-  const appConfig = useAppConfig();
-  const runtimeConfig = useRuntimeConfig();
+  if (window.location.hostname !== "localhost") {
+    const appConfig = useAppConfig();
+    const runtimeConfig = useRuntimeConfig();
 
-  const APPLICATION_ID = runtimeConfig.public.RUM_APP_MONITOR_ID;
-  const APPLICATION_VERSION = appConfig.APPLICATION_VERSION;
-  const APPLICATION_REGION = "ap-northeast-1";
+    const APPLICATION_ID = runtimeConfig.public.RUM_APP_MONITOR_ID;
+    const APPLICATION_VERSION = appConfig.APPLICATION_VERSION;
+    const APPLICATION_REGION = "ap-northeast-1";
 
-  new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, {
-    sessionSampleRate: 1,
-    endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
-    telemetries: ["performance", "errors", "http"],
-    identityPoolId: runtimeConfig.public.RUM_IDPOOL_ID,
-    allowCookies: false,
-    enableXRay: false,
-  });
+    new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, {
+      sessionSampleRate: 1,
+      endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
+      telemetries: ["performance", "errors", "http"],
+      identityPoolId: runtimeConfig.public.RUM_IDPOOL_ID,
+      allowCookies: false,
+      enableXRay: false,
+    });
+  }
 });
