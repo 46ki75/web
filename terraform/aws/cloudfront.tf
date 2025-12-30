59d11041561b2f3cf9d8eb36dd02ac98af588b45
Ikuma Yamashita <me@ikuma.cloud>
2025-12-30 17:00:07 +0000

    feat: add function association for viewer-request in CloudFront distribution

--- terraform/aws/cloudfront.tf
+++ terraform/aws/cloudfront.tf
@@ -293,6 +293,11 @@ resource "aws_cloudfront_distribution" "default" {
     response_headers_policy_id = aws_cloudfront_response_headers_policy.security.id
 
     compress = true
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.rename_uri.arn
+    }
   }
 
   origin {
