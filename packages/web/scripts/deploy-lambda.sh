7fb42b44a1ff5b41a7cf006c401ec9a86f7c223b
Ikuma Yamashita <me@ikuma.cloud>
2025-12-30 02:19:33 +0000

    feat: add Nitro Lambda function and associated IAM roles, policies, and CloudWatch log groups

---
+++ packages/web/scripts/deploy-lambda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+FUNCTION_NAME="${STAGE_NAME}-46ki75-web-lambda-function-nitro"
+ARTIFACT_PATH="../.output/server/"
+
+TEMPDIR=$(mktemp -d)
+trap "rm -rf ${TEMPDIR}" EXIT
+
+cp -r "${ARTIFACT_PATH}"/* "${TEMPDIR}/"
+
+zip -j -r "${TEMPDIR}/lambda.zip" "${TEMPDIR}/"*
+
+aws lambda update-function-code --function-name "${FUNCTION_NAME}" --zip-file "fileb://${TEMPDIR}/lambda.zip" --publish
+
+aws lambda update-aliase --function-name "${FUNCTION_NAME}" --name "stable" --function-version "\$LATEST"
+
+echo "Deployed Lambda function: ${FUNCTION_NAME}"
