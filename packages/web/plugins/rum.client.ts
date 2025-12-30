642b9756be6700b454f7dbeec8a6bae389acf631
Ikuma Yamashita <me@ikuma.cloud>
2025-12-30 07:18:37 +0000

    fix: improve hostname check for RUM client initialization

--- packages/web/plugins/rum.client.ts
+++ packages/web/plugins/rum.client.ts
@@ -1,7 +1,10 @@
 import { AwsRum } from "aws-rum-web";
 
 export default defineNuxtPlugin(async () => {
-  if (window.location.hostname !== "localhost") {
+  if (
+    window.location.hostname !== "localhost" &&
+    window.location.hostname !== "127.0.0.1"
+  ) {
     const appConfig = useAppConfig();
     const runtimeConfig = useRuntimeConfig();
 
