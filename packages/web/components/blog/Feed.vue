5e42016ae3cb1d9088102562eb71e2015f5eead7
Ikuma Yamashita <me@ikuma.cloud>
2026-01-31 11:29:39 +0000

    fix: update feed URLs in Feed component to use the correct API paths

--- packages/web/components/blog/Feed.vue
+++ packages/web/components/blog/Feed.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="icon-box">
-    <a :href="`/feed/blog/feed/rss/${language}`" target="_blank" class="a">
+    <a :href="`/api/v2/blog/feed/rss/${language}`" target="_blank" class="a">
       <Icon class="icon" icon="mdi:rss" />
       <span class="label">RSS</span>
     </a>
-    <a :href="`/feed/blog/feed/atom`" target="_blank" class="a">
+    <a :href="`/api/v2/blog/feed/atom`" target="_blank" class="a">
       <Icon class="icon" icon="mdi:atom" />
       <span class="label">Atom</span>
     </a>
-    <a :href="`/feed/blog/feed/json`" target="_blank" class="a">
+    <a :href="`/api/v2/blog/feed/json`" target="_blank" class="a">
       <Icon class="icon" icon="mdi:code-json" />
       <span class="label">Feed</span>
     </a>
