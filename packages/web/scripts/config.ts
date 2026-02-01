e4e5a01cd5f4b32b692a85bc6b39d0d5492e517a
Ikuma Yamashita <me@ikuma.cloud>
2026-02-01 10:22:50 +0000

    fix: improve STAGE_NAME handling and default to window location for endpoint

--- packages/web/scripts/config.ts
+++ packages/web/scripts/config.ts
@@ -1,17 +1,15 @@
-export const STAGE_NAME = process.env.STAGE_NAME ?? "dev";
-
-if (!["dev", "stg", "prod"].includes(STAGE_NAME)) {
-  throw new Error("STAGE_NAME is not valid.");
-}
+export const STAGE_NAME = process?.env?.STAGE_NAME;
 
 export const ENDPOINT =
-  STAGE_NAME === "prod"
-    ? `https://www.ikuma.cloud`
-    : `https://${STAGE_NAME}-www.ikuma.cloud`;
+  STAGE_NAME != null
+    ? STAGE_NAME === "prod"
+      ? `https://www.ikuma.cloud`
+      : `https://${STAGE_NAME}-www.ikuma.cloud`
+    : window.location.origin;
 
 export const GTAG =
   STAGE_NAME === "prod"
     ? "G-TW1BVM24YT"
     : STAGE_NAME === "stg"
-    ? "G-Q7K53RM4VC"
-    : "G-85QSG3WH5F";
+      ? "G-Q7K53RM4VC"
+      : "G-85QSG3WH5F";
