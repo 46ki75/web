eb3cf7844ac2ffce0b5b20d1a1144e7770001199
Ikuma Yamashita <me@ikuma.cloud>
2026-02-21 13:32:55 +0000

    feat: add async retry mechanism for executing functions with configurable retries

--- crates/web-lambda-cache-warmer/src/util.rs
+++ crates/web-lambda-cache-warmer/src/util.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use base64::prelude::*;
 
 pub fn get_base_domain(stage_name: &str) -> String {
@@ -11,4 +13,28 @@ pub fn create_basic_auth_header_value(username: &str, password: &str) -> String 
     let credentials = format!("{}:{}", username, password);
     let encoded_credentials = BASE64_STANDARD.encode(credentials);
     format!("Basic {}", encoded_credentials)
+}
+
+pub async fn execute_with_retry<MakeFut, Fut, O, E>(
+    async_function: MakeFut,
+    max_retries: u32,
+) -> Result<O, E>
+where
+    MakeFut: Fn() -> Fut,
+    Fut: std::future::Future<Output = Result<O, E>>,
+{
+    let mut attempt: u32 = 0;
+    loop {
+        match async_function().await {
+            Ok(result) => return Ok(result),
+            Err(e) => {
+                attempt += 1;
+                if attempt > max_retries {
+                    return Err(e);
+                }
+            }
+        }
+
+        tokio::time::sleep(Duration::from_secs(2_u64.pow(attempt))).await;
+    }
 }
