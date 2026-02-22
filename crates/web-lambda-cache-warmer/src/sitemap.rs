2dc471f38939336223a1d097f823b7952805cdb0
Ikuma Yamashita <me@ikuma.cloud>
2026-02-22 00:30:32 +0000

    feat: add sitemap parsing functionality and update dependencies

--- crates/web-lambda-cache-warmer/src/sitemap.rs
+++ crates/web-lambda-cache-warmer/src/sitemap.rs
@@ -1,9 +1,58 @@
-fn extract_sitemap_url(robotstxt: &str) -> Option<&str> {
+use std::{io::Cursor, str::FromStr};
+
+use reqwest::Url;
+use sitemap::reader::{SiteMapEntity, SiteMapReader};
+
+fn extract_sitemap_url_from_robots(robotstxt: &str) -> Option<&str> {
     let re = regex::Regex::new(r#"(?m)^Sitemap:\s(https://.*?)$"#).unwrap();
 
     re.captures(robotstxt)
         .and_then(|captures| captures.get(1))
         .map(|m| m.as_str())
+}
+
+pub async fn parse_sitemap(sitemap_url: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut urls = Vec::new();
+    let mut sitemap_entries = Vec::new();
+    let mut errors = Vec::new();
+
+    let entry_point = sitemap::structs::SiteMapEntry {
+        loc: sitemap::structs::Location::Url(Url::from_str(sitemap_url)?),
+        lastmod: sitemap::structs::LastMod::None,
+    };
+
+    sitemap_entries.push(entry_point);
+
+    let client = reqwest::Client::new();
+
+    while let Some(sitemap_entry) = sitemap_entries.pop() {
+        let url = match sitemap_entry.loc.get_url() {
+            Some(url) => url,
+            None => break,
+        };
+
+        let sitemap = client.get(url).send().await?.text().await?;
+
+        let parser = SiteMapReader::new(Cursor::new(sitemap.as_bytes()));
+
+        for entity in parser {
+            match entity {
+                SiteMapEntity::Url(url_entry) => {
+                    if let Some(url) = url_entry.loc.get_url() {
+                        urls.push(url.as_str().to_owned());
+                    };
+                }
+                SiteMapEntity::SiteMap(sitemap_entry) => {
+                    sitemap_entries.push(sitemap_entry);
+                }
+                SiteMapEntity::Err(error) => {
+                    errors.push(error);
+                }
+            };
+        }
+    }
+
+    Ok(urls)
 }
 
 #[cfg(test)]
@@ -20,7 +69,7 @@ Allow: /api/v2/blog/sitemap.xml
 Sitemap: https://dev-www.ikuma.cloud/sitemap-index.xml
         "#;
 
-        let sitemap_url = extract_sitemap_url(robotstxt);
+        let sitemap_url = extract_sitemap_url_from_robots(robotstxt);
 
         assert_eq!(
             sitemap_url,
