c4d16839cba3b3cd1c9249836f83f8ab420e1e0e
Ikuma Yamashita <me@ikuma.cloud>
2026-03-02 01:07:37 +0900

    feat: update Header component to include logo and link

--- packages/web-qwik/src/components/common/header.tsx
+++ packages/web-qwik/src/components/common/header.tsx
@@ -1,8 +1,17 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 
 import styles from "./header.scoped.scss?inline";
+import { Link } from "@builder.io/qwik-city";
+
+import Favicon from "../../../public/static/brand/logo.svg?jsx";
 
 export const Header = component$(() => {
   useStylesScoped$(styles);
-  return <header class="header">HEADER</header>;
+  return (
+    <header class="header">
+      <Link href="/">
+        <Favicon style={{ height: "1.5rem" }} />
+      </Link>
+    </header>
+  );
 });
