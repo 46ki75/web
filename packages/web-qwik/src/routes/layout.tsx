23793dc6bf0f26a726618344181d0649ead6d570
Ikuma Yamashita <me@ikuma.cloud>
2026-03-02 02:49:29 +0900

    refactor: remove unused view transition styles from layout component

--- packages/web-qwik/src/routes/layout.tsx
+++ packages/web-qwik/src/routes/layout.tsx
@@ -1,22 +1,7 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
 import { Header } from "~/components/common/header";
 
 export default component$(() => {
-  useStyles$(`
-    ::view-transition-old(root) {
-      animation: fade-out 200ms ease-out;
-    }
-    ::view-transition-new(root) {
-      animation: fade-in 200ms ease-in;
-    }
-    @keyframes fade-out {
-      to { opacity: 0; }
-    }
-    @keyframes fade-in {
-      from { opacity: 0; }
-    }
-  `);
-
   return (
     <>
       <Header />
