7769f76c5419daaa7c053098ed8e7a29d35fc2b5
Ikuma Yamashita <me@ikuma.cloud>
2026-03-03 01:02:40 +0900

    refactor: add ElmParallax component for enhanced background imagery

--- packages/web-qwik/src/routes/layout.tsx
+++ packages/web-qwik/src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { ElmParallax } from "@elmethis/qwik";
 import { Header } from "~/components/common/header";
 
 export default component$(() => {
@@ -8,6 +9,13 @@ export default component$(() => {
       <div class="routing-transition">
         <Slot />
       </div>
+
+      <ElmParallax
+        images={[
+          "/static/image/bg-crimson.webp",
+          "/static/image/bg-amber.webp",
+        ]}
+      />
     </>
   );
 });
