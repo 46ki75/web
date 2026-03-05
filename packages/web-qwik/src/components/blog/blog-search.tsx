830014657483ebca875bdb8491c08f4b20608a82
Ikuma Yamashita <me@ikuma.cloud>
2026-03-05 09:49:33 +0900

    feat: enhance blog search functionality with Fuse integration and add worker file

--- packages/web-qwik/src/components/blog/blog-search.tsx
+++ packages/web-qwik/src/components/blog/blog-search.tsx
@@ -1,4 +1,11 @@
-import { component$, useContext, useSignal, useTask$ } from "@builder.io/qwik";
+import {
+  component$,
+  noSerialize,
+  NoSerialize,
+  useContext,
+  useSignal,
+  useTask$,
+} from "@builder.io/qwik";
 
 import styles from "./blog-search.module.scss";
 import { BlogContext } from "~/context/blog";
@@ -17,23 +24,31 @@ export const BlogSearch = component$<BlogSearchProps>(({ language }) => {
 
   const searchKeyword = useSignal("");
   const searchResults = useSignal<(typeof blogState.blogMeta)[Language]>([]);
+  const fuseInstance = useSignal<NoSerialize<
+    Fuse<(typeof blogState.blogMeta)[Language][number]>
+  > | null>(null);
 
   useTask$(({ track }) => {
     track(() => searchKeyword.value);
     track(() => language);
 
-    if (!(searchKeyword.value.trim() === "")) {
-      console.log("searching", searchKeyword.value);
-      console.log("blogMeta", blogState.blogMeta[language]);
-      const fuse = new Fuse(blogState.blogMeta[language], {
-        keys: [
-          { name: "title", weight: 0.7 },
-          { name: "description", weight: 0.3 },
-          { name: "keywords", weight: 1 },
-        ],
-      });
+    if (fuseInstance.value == null) {
+      fuseInstance.value = noSerialize(
+        new Fuse(blogState.blogMeta[language], {
+          keys: [
+            { name: "title", weight: 0.7 },
+            { name: "description", weight: 0.3 },
+            { name: "keywords", weight: 1 },
+          ],
+          threshold: 0.3,
+        }),
+      );
+    }
 
-      const results = fuse.search(searchKeyword.value)?.map(({ item }) => item);
+    if (!(searchKeyword.value.trim() === "") && fuseInstance.value != null) {
+      const results = fuseInstance.value
+        .search(searchKeyword.value, { limit: 10 })
+        ?.map(({ item }) => item);
 
       searchResults.value = results;
     } else {
