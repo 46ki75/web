bca1e6228276cc51cd96f7ac2708d36ed8db205a
Ikuma Yamashita <me@ikuma.cloud>
2026-03-07 07:39:34 +0900

    feat: enhance BlogSide component with navigation button for search functionality

--- packages/web-qwik/src/components/blog/blog-side.tsx
+++ packages/web-qwik/src/components/blog/blog-side.tsx
@@ -1,11 +1,13 @@
-import { component$, useContext } from "@builder.io/qwik";
+import { $, component$, useContext } from "@builder.io/qwik";
 
 import styles from "./blog-side.module.scss";
 
-import { Link } from "@builder.io/qwik-city";
+import { Link, useNavigate } from "@builder.io/qwik-city";
 import { BlogContext } from "~/context/blog";
 import { Language } from "~/types";
 import { BlogCard } from "./blog-card";
+import { ElmButton, ElmInlineText, ElmMdiIcon } from "@elmethis/qwik";
+import { mdiBookSearch } from "@mdi/js";
 
 export type BlogSideProps = {
   language: Language;
@@ -13,14 +15,22 @@ export type BlogSideProps = {
 
 export const BlogSide = component$<BlogSideProps>(({ language }) => {
   const blogState = useContext(BlogContext);
+  const nav = useNavigate();
 
   return (
     <nav class={styles["blog-side"]}>
       <Link
         href={language === "en" ? "/blog/search" : `/${language}/blog/search`}
+      ></Link>
+
+      <ElmButton
+        onClick$={$(() =>
+          nav(language === "en" ? "/blog/search" : `/${language}/blog/search`),
+        )}
       >
-        SEARCH (TODO)
-      </Link>
+        <ElmMdiIcon d={mdiBookSearch} />
+        <ElmInlineText>Search</ElmInlineText>
+      </ElmButton>
 
       {blogState.blogMeta[language]?.map((blog, index) => (
         <BlogCard
