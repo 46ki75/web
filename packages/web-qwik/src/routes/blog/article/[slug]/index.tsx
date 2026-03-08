033773b06a1930b3da57b93d442589c76713b27e
Ikuma Yamashita <me@ikuma.cloud>
2026-03-08 22:01:38 +0900

    feat(blog): implement blog metadata loading and SEO enhancements in article component

--- packages/web-qwik/src/routes/blog/article/[slug]/index.tsx
+++ packages/web-qwik/src/routes/blog/article/[slug]/index.tsx
@@ -1,7 +1,63 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, isServer } from "@builder.io/qwik";
 
-import { useLocation } from "@builder.io/qwik-city";
+import { DocumentHead, routeLoader$, useLocation } from "@builder.io/qwik-city";
 import { BlogArticle } from "~/components/blog/blog-article";
+import { client } from "../../../../../openapi/client";
+
+export const useBlogMeta = routeLoader$(async ({ params, pathname }) => {
+  const { slug } = params;
+  const language = pathname.split("/")[1] === "ja" ? "ja" : "en";
+
+  const { data: blogMeta } = await client.GET("/api/v2/blog/{slug}", {
+    params: {
+      header: { "accept-language": language },
+      path: { slug },
+    },
+  });
+
+  return blogMeta && { language, meta: blogMeta.meta };
+});
+
+export const head: DocumentHead = ({ resolveValue }) => {
+  const origin = () => {
+    if (isServer) {
+      const stageName = process.env.STAGE_NAME;
+      const DOMAIN =
+        stageName === "prod"
+          ? "https://www-ikuma.cloud"
+          : `https://${stageName}-www.ikuma.cloud`;
+      return DOMAIN;
+    } else {
+      return location.origin;
+    }
+  };
+
+  const blogMeta = resolveValue(useBlogMeta);
+
+  return {
+    title: blogMeta ? blogMeta.meta.title : "Blog Article",
+    meta: [
+      {
+        name: "description",
+        content: blogMeta ? blogMeta.meta.description : "Blog Article",
+      },
+      {
+        property: "og:title",
+        content: blogMeta ? blogMeta.meta.title : "Blog Article",
+      },
+      {
+        property: "og:description",
+        content: blogMeta ? blogMeta.meta.description : "Blog Article",
+      },
+      {
+        property: "og:image",
+        content: blogMeta
+          ? `${origin()}/api/v2/blog/${blogMeta.meta.slug}/og-image?lang=${blogMeta.language}`
+          : "",
+      },
+    ],
+  };
+};
 
 export default component$(() => {
   const loc = useLocation();
