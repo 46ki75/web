eb7b18de8bf9fd8b97baa77301526101d57f34ed
Ikuma Yamashita <me@ikuma.cloud>
2026-03-08 22:50:43 +0900

    fix(blog): streamline blog metadata retrieval and improve SEO properties

--- packages/web-qwik/src/routes/blog/article/[slug]/index.tsx
+++ packages/web-qwik/src/routes/blog/article/[slug]/index.tsx
@@ -4,18 +4,17 @@ import { DocumentHead, routeLoader$, useLocation } from "@builder.io/qwik-city";
 import { BlogArticle } from "~/components/blog/blog-article";
 import { client } from "../../../../../openapi/client";
 
-export const useBlogMeta = routeLoader$(async ({ params, pathname }) => {
-  const { slug } = params;
-  const language = pathname.split("/")[1] === "ja" ? "ja" : "en";
+const LANGUAGE = "en";
 
+export const useBlogMeta = routeLoader$(async ({ params }) => {
   const { data: blogMeta } = await client.GET("/api/v2/blog/{slug}", {
     params: {
-      header: { "accept-language": language },
-      path: { slug },
+      header: { "accept-language": LANGUAGE },
+      path: { slug: params.slug },
     },
   });
 
-  return blogMeta && { language, meta: blogMeta.meta };
+  return blogMeta?.meta;
 });
 
 export const head: DocumentHead = ({ resolveValue }) => {
@@ -35,24 +34,24 @@ export const head: DocumentHead = ({ resolveValue }) => {
   const blogMeta = resolveValue(useBlogMeta);
 
   return {
-    title: blogMeta ? blogMeta.meta.title : "Blog Article",
+    title: blogMeta ? blogMeta.title : "Blog Article",
     meta: [
       {
         name: "description",
-        content: blogMeta ? blogMeta.meta.description : "Blog Article",
+        content: blogMeta ? blogMeta.description : "Blog Article",
       },
       {
         property: "og:title",
-        content: blogMeta ? blogMeta.meta.title : "Blog Article",
+        content: blogMeta ? blogMeta.title : "Blog Article",
       },
       {
         property: "og:description",
-        content: blogMeta ? blogMeta.meta.description : "Blog Article",
+        content: blogMeta ? blogMeta.description : "Blog Article",
       },
       {
         property: "og:image",
         content: blogMeta
-          ? `${origin()}/api/v2/blog/${blogMeta.meta.slug}/og-image?lang=${blogMeta.language}`
+          ? `${origin()}/api/v2/blog/${blogMeta.slug}/og-image?lang=${LANGUAGE}`
           : "",
       },
     ],
