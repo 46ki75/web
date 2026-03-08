9986cce1a76bde1c18cd5cf76ea26712c453af97
Ikuma Yamashita <me@ikuma.cloud>
2026-03-09 00:27:16 +0900

    feat(blog): refactor BlogIndex component to enhance structure and add navigation metadata

--- packages/web-qwik/src/components/blog/blog-index.tsx
+++ packages/web-qwik/src/components/blog/blog-index.tsx
@@ -1,28 +1,53 @@
-import { component$, useContext } from "@builder.io/qwik";
+import { $, component$, useContext } from "@builder.io/qwik";
 
 import styles from "./blog-index.module.scss";
 import { BlogContext } from "~/context/blog";
 import { Language } from "~/types";
 import { BlogCard } from "./blog-card";
+import { Meta } from "../common/meta";
+import { useNavigate } from "@builder.io/qwik-city";
 
 export interface BlogIndexProps {
   language: Language;
 }
 
 export const BlogIndex = component$<BlogIndexProps>(({ language }) => {
+  const nav = useNavigate();
   const blogState = useContext(BlogContext);
 
   return (
     <div class={styles["blog-index"]}>
-      {blogState.blogMeta[language].map((blog, index) => (
-        <BlogCard
-          key={blog.page_id}
-          blog={blog}
-          tags={blogState.tags?.filter((tag) => blog.tag_ids?.includes(tag.id))}
-          language={language}
-          delay={(index + 1) * 100}
-        ></BlogCard>
-      ))}
+      <Meta
+        title="Blog"
+        createdAt="2023-10-01"
+        updatedAt="2023-10-01"
+        links={[
+          {
+            text: "Home",
+            onClick$: $(() => nav(language === "en" ? "/" : `/${language}`)),
+          },
+          {
+            text: "Blog",
+            onClick$: $(() =>
+              nav(language === "en" ? "/blog" : `/${language}/blog`),
+            ),
+          },
+        ]}
+      />
+
+      <div class={styles["blog-card-list"]}>
+        {blogState.blogMeta[language].map((blog, index) => (
+          <BlogCard
+            key={blog.page_id}
+            blog={blog}
+            tags={blogState.tags?.filter((tag) =>
+              blog.tag_ids?.includes(tag.id),
+            )}
+            language={language}
+            delay={(index + 1) * 100}
+          ></BlogCard>
+        ))}
+      </div>
     </div>
   );
 });
