7af310fe6ac38b2dbf1381657032a6ef277fa4c7
Ikuma Yamashita <me@ikuma.cloud>
2026-03-12 19:48:39 +0900

    refactor(layer-decoration): restructure component layout and update styles for improved design

--- packages/web-qwik/src/routes/index.tsx
+++ packages/web-qwik/src/routes/index.tsx
@@ -1,32 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
-import { LinkLocale } from "~/components/common/link-locale";
 
-import { Meta } from "~/components/common/meta";
 import { LayerDecoration } from "~/components/main/layer-decoration";
 
 export default component$(() => {
   return (
     <div>
-      <div style={{ width: "800px", margin: "0 auto" }}>
-        <Meta
-          title="Welcome to Qwik"
-          createdAt="2024-01-01"
-          updatedAt="2024-06-01"
-          image="https://www.ikuma.cloud/api/v2/blog/leather-shoes-painting/og-image?lang=en"
-          links={[
-            {
-              text: "Home",
-            },
-          ]}
-        />
-
-        <LinkLocale href="/blog/article/leather-shoes-painting/">
-          <span style={{ fontSize: 64 }}>BLOG Sample</span>
-        </LinkLocale>
-
-        <LayerDecoration />
-      </div>
+      <LayerDecoration />
     </div>
   );
 });
