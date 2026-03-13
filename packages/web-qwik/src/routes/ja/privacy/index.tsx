00e32bad51e4fb06fb014769208170ffea24ba8d
Ikuma Yamashita <me@ikuma.cloud>
2026-03-14 00:36:16 +0900

    feat(seo): add Japanese privacy policy route with localized metadata

--- packages/web-qwik/src/routes/ja/privacy/index.tsx
+++ packages/web-qwik/src/routes/ja/privacy/index.tsx
@@ -1,6 +1,25 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import { Privacy } from "~/components/main/privacy";
+import { generateHead } from "~/utils/common";
 
 export default component$(() => {
   return <Privacy language="ja" />;
 });
+
+export const useUrl = routeLoader$(({ url }) => url.toString());
+
+export const head: DocumentHead = ({ resolveValue }) => {
+  const url = resolveValue(useUrl);
+
+  const headBase = generateHead({
+    url,
+    title: "プライバシーポリシー",
+    ogType: "profile",
+    description: "ポートフォリオとブログ",
+    language: "ja",
+  });
+
+  return headBase;
+};
