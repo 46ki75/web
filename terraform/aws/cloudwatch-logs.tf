9e8c7d7b4c0cebca83c10ad44ab83562a02bc211
Ikuma Yamashita <me@ikuma.cloud>
2026-03-29 20:36:47 +0900

    feat: add CloudWatch log subscription filters for HTTP WARN and ERROR levels

--- terraform/aws/cloudwatch-logs.tf
+++ terraform/aws/cloudwatch-logs.tf
@@ -18,3 +18,16 @@ resource "aws_cloudwatch_log_group" "lambda_reporter" {
   retention_in_days = 30
 }
 
+resource "aws_cloudwatch_log_subscription_filter" "lambda_http_warn" {
+  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_http_warn"
+  log_group_name  = aws_cloudwatch_log_group.lambda_http.name
+  filter_pattern  = "{$.level=\"WARN\"}"
+  destination_arn = aws_lambda_function.reporter.arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "lambda_http_error" {
+  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_http_error"
+  log_group_name  = aws_cloudwatch_log_group.lambda_http.name
+  filter_pattern  = "{$.level=\"ERROR\"}"
+  destination_arn = aws_lambda_function.reporter.arn
+}
